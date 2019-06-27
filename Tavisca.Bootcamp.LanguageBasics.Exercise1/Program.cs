654908a579501fd6f64807615b0efd635b53a5e0
Gursimran Singh <gsingh@tavisca.in>
2019-06-27 22:01:09 +0530

    Completed FindDigit Function

--- Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
@@ -6,6 +6,7 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
     {
         static void Main(string[] args)
         {
+        
             Test("42*47=1?74", 9);
             Test("4?*47=1974", 2);
             Test("42*?7=1974", 4);
@@ -22,7 +23,52 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
 
         public static int FindDigit(string equation)
         {
+
             // Add your code here.
+            var numbers=equation.Split("=")[0];
+            var result=equation.Split("=")[1];
+            var firstNumber=numbers.Split("*")[0];
+            var secondNumber=numbers.Split("*")[1];
+            var postition=0;
+            var requiredNumber=0;
+            var missingDigit=0;
+            var product=0;
+            if(firstNumber.Contains("?")){
+                
+                requiredNumber=Convert.ToInt32(result)/Convert.ToInt32(secondNumber);
+                postition=firstNumber.IndexOf("?");
+                missingDigit=requiredNumber.ToString()[postition];
+                product=requiredNumber*Convert.ToInt32(secondNumber);
+                if (requiredNumber.ToString().Length!=firstNumber.Length){
+                    return -1;
+                }
+
+            }else if (secondNumber.Contains("?")){
+                requiredNumber=Convert.ToInt32(result)/Convert.ToInt32(firstNumber);
+                postition=secondNumber.IndexOf("?");
+                missingDigit=requiredNumber.ToString()[postition];
+                product=requiredNumber*Convert.ToInt32(firstNumber);
+                if (requiredNumber.ToString().Length!=secondNumber.Length){
+                    return -1;
+                }
+                
+            }else{
+                requiredNumber=Convert.ToInt32(firstNumber)*Convert.ToInt32(secondNumber);
+                postition=result.IndexOf("?");
+                missingDigit=requiredNumber.ToString()[postition];
+                product=requiredNumber;
+                return (int)Char.GetNumericValue(requiredNumber.ToString()[postition]);
+            }
+            if(postition==0 && (int)Char.GetNumericValue(requiredNumber.ToString()[postition])==0){
+                    return -1;}
+            else{
+                if(product!=Convert.ToInt32(result)){
+                    return -1;
+                }else{
+                    return (int)Char.GetNumericValue(requiredNumber.ToString()[postition]);
+                }
+            }            
+
             throw new NotImplementedException();
         }
     }
